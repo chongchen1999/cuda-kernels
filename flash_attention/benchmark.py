2eaa3c6a412106d4e4329a941bb0acc2f9dd1958
Chong Chen <chongchen1999@gmail.com>
2024-08-22 15:45:39 -0400

    add flash attention

--- flash_attention/benchmark.py
+++ flash_attention/benchmark.py
@@ -4,36 +4,43 @@ import torch
 from torch.nn import functional as F
 from torch.utils.cpp_extension import load
 
-# Load the CUDA kernel as a python module
-minimal_attn = load(name='minimal_attn', sources=['main.cpp', 'flash.cu'], extra_cuda_cflags=['-O2'])
+print('loading')
+
+# Build shared library for loading the CUDA kernel as a python module
+flash_attention = load(name='flash_attention', sources=['main.cpp', 'flash_attention.cu'], extra_cuda_cflags=['-O2'])
 
 # Use small model params, otherwise slower than manual attention. See caveats in README.
 batch_size = 16
-n_head = 12
+head_num = 12
 seq_len = 64
-head_embd = 64
+head_size = 64
 
-q = torch.randn(batch_size, n_head, seq_len, head_embd).cuda()
-k = torch.randn(batch_size, n_head, seq_len, head_embd).cuda()
-v = torch.randn(batch_size, n_head, seq_len, head_embd).cuda()
+print('loaded!')
+
+q = torch.randn(batch_size, head_num, seq_len, head_size).cuda()
+k = torch.randn(batch_size, head_num, seq_len, head_size).cuda()
+v = torch.randn(batch_size, head_num, seq_len, head_size).cuda()
+
+def manual_attention(q, k, v):
+    att = torch.matmul(q, k.transpose(-2, -1)).mul_(1.0 / math.sqrt(k.size(-1)))
+    att = F.softmax(att, dim=-1)
+    att = torch.matmul(att, v)
+    return att
 
 print('=== profiling manual attention ===')
 
-# Our minimal flash attention aims to be faster than this by avoiding HBM read/writes of N^2 matrices.
-def manual_attn(q, k, v):
-    att = (q @ k.transpose(-2, -1) * (1.0 / math.sqrt(k.size(-1))))
-    att = F.softmax(att, dim=-1)
-    y = att @ v
-    return y
-
-with torch.autograd.profiler.profile(use_cuda=True) as prof:
-    manual_result = manual_attn(q, k, v)
+with torch.autograd.profiler.profile(use_device='cuda') as prof:
+    manual_result = manual_attention(q, k, v)
 print(prof.key_averages().table(sort_by='cuda_time_total', row_limit=10))
 
-print('=== profiling minimal flash attention === ')
 
-with torch.autograd.profiler.profile(use_cuda=True) as prof:
-    minimal_result = minimal_attn.forward(q, k, v)
+print('=== profiling flash attention === ')
+
+with torch.autograd.profiler.profile(use_device='cuda') as prof:
+    flash_attention_result = flash_attention.forward(q, k, v)
 print(prof.key_averages().table(sort_by='cuda_time_total', row_limit=10))
 
-print('attn values sanity check:', torch.allclose(minimal_result, manual_result, rtol=0, atol=1e-02))
+# print(manual_result)
+# print(flash_attention_result)
+
+print('attn values sanity check:', torch.allclose(flash_attention_result, manual_result, rtol=0, atol=1e-02))
